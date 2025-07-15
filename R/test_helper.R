374af0fb157a12901cb6dc6ff52fd07579670be8
tucano <davide.rambaldi@gmail.com>
2025-07-15 10:24:19 +0200

    fixtures for stats and data

--- R/test_helper.R
+++ R/test_helper.R
@@ -4,28 +4,47 @@
 #'
 #' @param root the root directory to build the structure dirs
 #'
-build_dir_structure <- function(root = getwd()) {
-  fragdir <- file.path(root, "results")
+build_dir_structure <- function(root = getwd()) {fragdir <- file.path(root, "results")
   dir.create(fragdir, showWarnings = FALSE)
 
   # build directory structure for samplesheet.csv
   samplesheet <- system.file("extdata",
                              "samplesheet.csv",
                              package = "fragmentomics")
+
   samples <- fragmentomics::parse_samplesheet(samplesheet)
 
   # CTCF and ELK4 example file
   ctcf_example_file <- system.file("extdata",
                                    "CTCF.gz",
                                    package = "fragmentomics")
+
   elk4_example_file <- system.file("extdata",
                                    "ELK4.gz",
                                    package = "fragmentomics")
+
+  ctcf_stats_file <- system.file("extdata",
+                                 "CTCF_peak_stats.tsv",
+                                 package = "fragmentomics")
+
+  elk4_stats_file <- system.file("extdata",
+                                 "ELK4_peak_stats.tsv",
+                                 package = "fragmentomics")
+
+  ctcf_data_file <- system.file("extdata",
+                                 "CTCF_peak_data.tsv",
+                                 package = "fragmentomics")
+
+  elk4_data_file <- system.file("extdata",
+                                 "ELK4_peak_data.tsv",
+                                 package = "fragmentomics")
 
   samples |> purrr::pmap(function(caseid,
                                   sampleid,
                                   timepoint,
                                   encoded_timepoint) {
+
+    # MATRIX
     mpath <- file.path(fragdir,
                        caseid,
                        sampleid,
@@ -34,8 +53,25 @@ build_dir_structure <- function(root = getwd()) {
                        "matrix",
                        "source_1",
                        "CTCF")
+
     dir.create(mpath, showWarnings = FALSE, recursive = TRUE)
     file.copy(ctcf_example_file, mpath, overwrite = TRUE)
+
+    # STATS AND DATA
+    mpath <- file.path(fragdir,
+                       caseid,
+                       sampleid,
+                       "fragmentomics",
+                       "processed",
+                       "peakstats",
+                       "source_1",
+                       "CTCF")
+
+    dir.create(mpath, showWarnings = FALSE, recursive = TRUE)
+    file.copy(ctcf_stats_file, mpath, overwrite = TRUE)
+    file.copy(ctcf_data_file, mpath, overwrite = TRUE)
+
+    # MATRIX
     mpath <- file.path(fragdir,
                        caseid,
                        sampleid,
@@ -44,8 +80,25 @@ build_dir_structure <- function(root = getwd()) {
                        "matrix",
                        "source_1",
                        "ELK4")
+
     dir.create(mpath, showWarnings = FALSE, recursive = TRUE)
     file.copy(elk4_example_file, mpath, overwrite = TRUE)
+
+    # STATS AND DATA
+    mpath <- file.path(fragdir,
+                       caseid,
+                       sampleid,
+                       "fragmentomics",
+                       "processed",
+                       "peakstats",
+                       "source_1",
+                       "ELK4")
+
+    dir.create(mpath, showWarnings = FALSE, recursive = TRUE)
+    file.copy(elk4_stats_file, mpath, overwrite = TRUE)
+    file.copy(elk4_data_file, mpath, overwrite = TRUE)
+
   })
+
   fragdir
 }
