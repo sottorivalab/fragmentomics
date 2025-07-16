d4bd71f962555f0d726176a3574bbf541a5ea68e
tucano <davide.rambaldi@gmail.com>
2025-07-16 11:15:46 +0200

    test load_samples.R

---
+++ tests/testthat/test-load_samples.R
@@ -0,0 +1,100 @@
+test_that("test load_samples from example dir structure", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
+
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+  samplesheet <- fragmentomics::parse_samplesheet(example_samplesheet)
+
+  results <- fragmentomics::load_samples(
+    caseid = samplesheet$caseid[1],
+    sampleid = samplesheet$sampleid[1],
+    timepoint = samplesheet$timepoint[1],
+    encoded_timepoint = samplesheet$encoded_timepoint[1],
+    rootpath = fragdir,
+    subdir = "fragmentomics/processed"
+  )
+
+  expect_true(tibble::is_tibble(results))
+})
+
+test_that("test load_samples fail with missing peak stats file", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
+
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+
+  samplesheet <- fragmentomics::parse_samplesheet(example_samplesheet)
+  file_to_remove <- file.path(fragdir, "SAMPLE_1/SAMPLE_1_BL/fragmentomics/processed/peakstats/source_1/CTCF/CTCF_peak_stats.tsv")
+  file.remove(file_to_remove)
+  expect_error(
+    fragmentomics::load_samples(
+      caseid = samplesheet$caseid[1],
+      sampleid = samplesheet$sampleid[1],
+      timepoint = samplesheet$timepoint[1],
+      encoded_timepoint = samplesheet$encoded_timepoint[1],
+      rootpath = fragdir,
+      subdir = "fragmentomics/processed"
+    ),
+    "peak_stats_file path does not exist"
+  )
+})
+
+test_that("test load_samples fail with missing peak data file", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
+
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+
+  samplesheet <- fragmentomics::parse_samplesheet(example_samplesheet)
+  file_to_remove <- file.path(fragdir, "SAMPLE_1/SAMPLE_1_BL/fragmentomics/processed/peakstats/source_1/CTCF/CTCF_peak_data.tsv")
+  file.remove(file_to_remove)
+  expect_error(
+    fragmentomics::load_samples(
+      caseid = samplesheet$caseid[1],
+      sampleid = samplesheet$sampleid[1],
+      timepoint = samplesheet$timepoint[1],
+      encoded_timepoint = samplesheet$encoded_timepoint[1],
+      rootpath = fragdir,
+      subdir = "fragmentomics/processed"
+    ),
+    "peak_data_file path does not exist"
+  )
+})
+
+test_that("test load_samples fail with missing matrix file", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
+
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+
+  samplesheet <- fragmentomics::parse_samplesheet(example_samplesheet)
+  file_to_remove <- file.path(fragdir, "SAMPLE_1/SAMPLE_1_BL/fragmentomics/processed/matrix/source_1/CTCF/CTCF.gz")
+  file.remove(file_to_remove)
+  expect_error(
+    fragmentomics::load_samples(
+      caseid = samplesheet$caseid[1],
+      sampleid = samplesheet$sampleid[1],
+      timepoint = samplesheet$timepoint[1],
+      encoded_timepoint = samplesheet$encoded_timepoint[1],
+      rootpath = fragdir,
+      subdir = "fragmentomics/processed"
+    ),
+    "matrix_file_name path does not exist"
+  )
+})
