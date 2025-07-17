a6038ff64143e9f2868f9c6cf3c51caabb504414
tucano <davide.rambaldi@gmail.com>
2025-07-17 10:27:05 +0200

    target plot test

--- tests/testthat/test-target_plot.R
+++ tests/testthat/test-target_plot.R
@@ -1,3 +1,16 @@
 test_that("target_plot return a ggplot", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
 
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+  samplesheet <- parse_samplesheet(example_samplesheet)
+  experiment <- load_experiment(samplesheet, fragdir)
+  ctcf <- load_peaks(experiment |> dplyr::filter(target == "CTCF"), fragdir)
+
+  g <- target_plot(ctcf)
+  expect_true(ggplot2::is_ggplot(g))
 })
