d97cb5b69cdea24944d3b4ea2cd84a8e6ea3ed0d
tucano <davide.rambaldi@gmail.com>
2025-07-24 12:06:38 +0200

    load_peaks test

--- tests/testthat/test-load_peaks.R
+++ tests/testthat/test-load_peaks.R
@@ -32,3 +32,19 @@ test_that("test load_peaks from missing dir structure", {
     "Root path does not exist: non_existent_dir"
   )
 })
+
+test_that("test load_peaks from example dir structure single process", {
+  tmpdir <- withr::local_tempdir()
+  fragdir <- build_dir_structure(root = tmpdir)
+
+  example_samplesheet <- system.file(
+    "extdata",
+    "samplesheet.csv",
+    package = "fragmentomics"
+  )
+  samplesheet <- parse_samplesheet(example_samplesheet)
+  experiment <- load_experiment(samplesheet, fragdir, parallelize = FALSE)
+  ctcf <- experiment |> dplyr::filter(target_label == "CTCF")
+  ctcf_peaks <- load_peaks(ctcf, fragdir)
+  expect_true(tibble::is_tibble(ctcf_peaks))
+})
